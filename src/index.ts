588646a4ad0dfbba286a24d60e2d118882cb7a1b
Mihir Verma <mihirv7781@gmail.com>
2024-12-30 19:49:53 +0530

    changes in ts configs

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface IColor {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: IColor = {
+  red: 0,
+  blue: 1,
+  green: 2,
+};
+
+console.log(color);
+
+export default color;
